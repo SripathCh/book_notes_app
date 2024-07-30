bd9e922f7288ca23b420a384345f582d7c64f514
SripathCh <sripath.cherukuri@gmail.com>
2024-07-30 16:51:27 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+// setting the app to express server and port number to 30000
+const app = express();
+const port = 3000;
+
+// using pg Client to connect to the database
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "postgres",
+    password: "StayPositive24",
+    port: 5432,
+});
+db.connect();
+
+// using the bodyparser and setting the static folder to public from where we get all 
+// the images and styling for the website
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
