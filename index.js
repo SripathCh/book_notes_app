7d9c76142f9705d74023a035351e175d24077e00
SripathCh <sripath.cherukuri@gmail.com>
2024-07-31 06:05:19 -0400

    favicon addition

--- index.js
+++ index.js
@@ -21,8 +21,67 @@ db.connect();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+// Dummy data for books
+const books = [
+    {
+      id: 1,
+      title: 'The Great Gatsby',
+      author: 'F. Scott Fitzgerald',
+      cover_url: 'https://example.com/great-gatsby.jpg',
+      rating: 5,
+      review: 'A classic piece of American fiction.',
+      date_read: '2023-01-01'
+    },
+    {
+        id: 1,
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        cover_url: 'https://example.com/great-gatsby.jpg',
+        rating: 5,
+        review: 'A classic piece of American fiction.',
+        date_read: '2023-01-01'
+      },
+      {
+        id: 1,
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        cover_url: 'https://example.com/great-gatsby.jpg',
+        rating: 5,
+        review: 'A classic piece of American fiction.',
+        date_read: '2023-01-01'
+      },
+      {
+        id: 1,
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        cover_url: 'https://example.com/great-gatsby.jpg',
+        rating: 5,
+        review: 'A classic piece of American fiction.',
+        date_read: '2023-01-01'
+      },
+      {
+        id: 1,
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        cover_url: 'https://example.com/great-gatsby.jpg',
+        rating: 5,
+        review: 'A classic piece of American fiction.',
+        date_read: '2023-01-01'
+      },
+      {
+        id: 1,
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        cover_url: 'https://example.com/great-gatsby.jpg',
+        rating: 5,
+        review: 'A classic piece of American fiction.',
+        date_read: '2023-01-01'
+      },
+    // Add more book objects here
+  ];
+
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {books});
   });
 
 // setting the app to listen on port 3000
